e6e5ae35ff6658e6cae45b40fd349ddde907dcfd
Martin <hello@northrook.com>
2024-09-08 09:05:44 +0100

    mend

--- src/Minify/HtmlMinifier.php
+++ src/Minify/HtmlMinifier.php
@@ -1,54 +1,11 @@
-<?php
 
-namespace Northrook\Minify;
-
-use Northrook\Minify;
-
-
-final class HtmlMinifier extends Minify
+/**
+* Trim unnecessary whitespace around brackets
+*/
+private function trimElementTagBrackets() : HtmlMinifier
 {
-
-    // private array $matchedElements = [];
-
-    protected function minifyString() : void {
-        $this->trimHtmlComments()
-             ->trimWhitespace()
-            // ->trimElementTagBrackets()
-        ;
-    }
-
-    /**
-     * Trim unnecessary whitespace around brackets
-     */
-    private function trimElementTagBrackets() : HtmlMinifier {
-        // \preg_replace(
-        /*    [ '#(^<\w+.*?>)\s+#', '#\s+(</\w+?>)$#', ], '$1', $rawLatteContent,*/
-        // )
-        $this->string = \preg_replace( '#\s*(<|>|\/>)\s*#m', '$1', $this->string );
-        return $this;
-    }
-
-    // TODO : In development
-    private function removeEmptyAttributes() : HtmlMinifier {
-        // foreach ( $this->matchHtmlOpeningElement() as $match ) {
-        //     if ( count( $match ) === 1 ) {
-        //         continue;
-        //     }
-        //     dump( $match );
-        // }
-        return $this;
-    }
-
-    // TODO : In development
-    // private function matchHtmlOpeningElement( bool $rescan = false ) : array {
-    //     if ( !$this->matchedElements || $rescan ) {
-    //         preg_match_all(
-    /*            '/(<\w.*? ((\w+?)=["\']["\'])*.+?>)/s',*/
-    //             $this->string,
-    //             $this->matchedElements,
-    //             PREG_SET_ORDER,
-    //         );
-    //     }
-    //     return $this->matchedElements;
-    // }
+$this->string = \preg_replace( [ '#(^<\w+.*?>)\s+#', '#\s+(</\w+?>)$#', ], '$1', $this->string );
+$this->string = \preg_replace( '# (<\/[bi]>) *?<#m', '$1 <', $this->string );
+$this->string = \preg_replace( '# (<\/\w+?>)<#m', '$1 <', $this->string );
+return $this;
 }
