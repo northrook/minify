c481034f32f7a18a27fb792ecb86adab87996912
Martin <hello@northrook.com>
2024-09-08 16:13:50 +0100

    mend

--- src/Minify/HtmlMinifier.php
+++ src/Minify/HtmlMinifier.php
@@ -2,53 +2,127 @@
 
 namespace Northrook\Minify;
 
+use JetBrains\PhpStorm\Language;
+use Northrook\HTML\HtmlNode;
+use Northrook\Interface\Printable;
 use Northrook\Minify;
+use Northrook\Trait\PrintableClass;
+use function Northrook\replaceEach;
+use const Northrook\EMPTY_STRING;
 
 
-final class HtmlMinifier extends Minify
+final class HtmlMinifier implements Printable
 {
+    use PrintableClass;
 
-    // private array $matchedElements = [];
 
-    protected function minifyString() : void {
-        $this->trimHtmlComments()
-             ->trimWhitespace()
-            ->trimElementTagBrackets()
-        ;
-    }
+    private string $string;
 
     /**
-     * Trim unnecessary whitespace around brackets
+     * The first of the characters currently being looked at.
+     *
+     * @var string | bool | null
      */
-    private function trimElementTagBrackets() : HtmlMinifier
+    protected string | bool | null $current = '';
+
+    /**
+     * The next character being looked at (after a);
+     *
+     * @var string | bool | null
+     */
+    protected string | bool | null $ahead = '';
+
+    /**
+     * This character is only active when certain look ahead actions take place.
+     *
+     * @var  string | bool | null
+     */
+    protected string | bool | null $lookAhead;
+
+    /**
+     * This character is only active when certain look ahead actions take place.
+     *
+     * @var  string | bool | null
+     */
+    protected string | bool | null $previous;
+
+    public function __construct(
+        #[Language( 'HTML' )]
+        string $string,
+    )
     {
+        $string = \preg_replace( '#^\h*?<!--.*?-->\R*#ms', '', $string );
+        $string = \preg_replace( '#\s+#', ' ', $string );
+
+        $this->string = \trim( $string, ' ' );
+
+        $this->domDocumentOperations();
+
         $this->string = \preg_replace( [ '#(^<\w+.*?>)\s+#', '#\s+(</\w+?>)$#', ], '$1', $this->string );
-        $this->string = \preg_replace( '# (<\/[bi]>) *?<#m', '$1 <', $this->string );
-        $this->string = \preg_replace( '# (<\/\w+?>)<#m', '$1 <', $this->string );
+        /*$this->string = \preg_replace( '#( +?<.+?>.*?) *?(<\/(?:a|b|i)>)#m', '$1$2', $this->string );*/
+        /*$this->string = \preg_replace( '#( +?<.+?>.*?) *?(<\/small>)#m', '$1$2', $this->string );*/
+        // $this->string = \preg_replace( '# (<\/[bi]>) *?<#m', '$1', $this->string );
+        /*/*$this->string = \preg_replace( '# (<\/\w+?>)<#m', '$1 <', $this->string );*/
+        /*$this->string = \preg_replace( '#<\/\w+?>(\s+?)<\/\w+?>#m', '', $this->string );*/
+    }
+
+    public function __toString() : string
+    {
+        return $this->string;
+    }
+
+    private function domDocumentOperations() : self
+    {
+        $html = new HtmlNode( $this->string );
+
+        $this->parseDomElements( $html->iterateChildNodes() );
+
+        $this->string = $html->html;
+
         return $this;
     }
 
-    // TODO : In development
-    private function removeEmptyAttributes() : HtmlMinifier {
-        // foreach ( $this->matchHtmlOpeningElement() as $match ) {
-        //     if ( count( $match ) === 1 ) {
-        //         continue;
-        //     }
-        //     dump( $match );
-        // }
-        return $this;
+    private function parseDomElements( \DOMNodeList $iterateChildNodes ) : void
+    {
+        foreach ( $iterateChildNodes as $node ) {
+            // if ( $node->previousSibling )
+
+            if ( $node->hasChildNodes() ) {
+                if ( $node->hasAttributes() ) {
+                    $this->parseElementAttributes( $node );
+                }
+
+                $this->parseDomElements( $node->childNodes );
+                continue;
+            }
+            // if ( $node instanceof \DOMText ) {
+            //     $this->parseWhitespace( $node );
+            // }
+
+        }
     }
 
-    // TODO : In development
-    // private function matchHtmlOpeningElement( bool $rescan = false ) : array {
-    //     if ( !$this->matchedElements || $rescan ) {
-    //         preg_match_all(
-    /*            '/(<\w.*? ((\w+?)=["\']["\'])*.+?>)/s',*/
-    //             $this->string,
-    //             $this->matchedElements,
-    //             PREG_SET_ORDER,
-    //         );
-    //     }
-    //     return $this->matchedElements;
-    // }
+    private function parseWhitespace( \DOMText $textNode ) : void
+    {
+        if ( !\trim( $textNode->textContent ) ) {
+            $textNode->nodeValue = EMPTY_STRING;
+            return;
+        }
+        dump(
+            $textNode->previousSibling?->nodeValue,
+            $textNode->nextSibling?->nodeValue,
+        );
+    }
+
+    private function parseElementAttributes( mixed $node ) : void
+    {
+        /** @var \DOMAttr $attribute */
+        foreach ( $node->attributes as $attribute ) {
+            $attribute->nodeValue = match ( $attribute->nodeName ) {
+                'id', 'class' => \trim( $attribute->nodeValue ),
+                'style'       => \trim( \preg_replace( '#\s*([:;])\s*#m', '$1', $attribute->nodeValue ) ),
+                default       => $attribute->nodeValue,
+            };
+        }
+    }
 }
